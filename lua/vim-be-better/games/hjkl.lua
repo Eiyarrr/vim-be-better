e6247a856c14f413da9162f255711b4384733c56
Eiyarrr <eiyar@eiyar.com>
2026-07-19 11:29:54 -0700

    feat: added local game to hjkl storing score and the board

--- lua/vim-be-better/games/hjkl.lua
+++ lua/vim-be-better/games/hjkl.lua
@@ -1,9 +1,17 @@
 --[[
     Literally the same as vim-be-good's hjkl
     Use 'H', 'J', 'K', 'L' to move around the terminal,
-    pressing 'X' when over the 'O' to remove it.
+    hover over the 'O' to remove it.
 --]]
 local ui = require("lua.vim-be-better.ui")
+
+local game = {
+    score = 0,
+    board = {
+        { ".", ".", ".", "O", "." },
+        { ".", "O", ".", ".", "." },
+    }
+}
 
 local function beginGame()
     print("HJKL Starting!")
