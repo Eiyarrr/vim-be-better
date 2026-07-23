17d20a1d7cb6048a57244a79e75be278fe2d7926
Eiyarrr <eiyar@eiyar.com>
2026-07-23 02:39:57 -0700

    fix: menu mappings now also delete buffer local mappings rather than trying to delete non existent global mappings

--- lua/vim-be-better/menu.lua
+++ lua/vim-be-better/menu.lua
@@ -29,14 +29,14 @@ end
 
 set_mappings_mode = function(buffer, on_complete)
     vim.keymap.set("n", "1", function()
-        del_mappings_mode()
+        del_mappings_mode(buffer)
         create_difficulty_menu(buffer, on_complete)
         mode = registry.hjkl
     end, { buffer = buffer })
 end
 
-del_mappings_mode = function()
-    vim.keymap.del("n", "1")
+del_mappings_mode = function(buffer)
+    vim.keymap.del("n", "1", { buffer = buffer })
 end
 
 create_difficulty_menu = function(buffer, on_complete)
@@ -59,28 +59,28 @@ end
 
 set_mappings_difficulty = function(buffer, on_complete)
     vim.keymap.set("n", "1", function()
-        del_mappings_difficulty()
+        del_mappings_difficulty(buffer)
         on_complete(mode, "easy")
     end, { buffer = buffer })
     vim.keymap.set("n", "2", function()
-        del_mappings_difficulty()
+        del_mappings_difficulty(buffer)
         on_complete(mode, "medium")
     end, { buffer = buffer })
     vim.keymap.set("n", "3", function()
-        del_mappings_difficulty()
+        del_mappings_difficulty(buffer)
         on_complete(mode, "hard")
     end, { buffer = buffer })
     vim.keymap.set("n", "4", function()
-        del_mappings_difficulty()
+        del_mappings_difficulty(buffer)
         on_complete(mode, "challenge")
     end, { buffer = buffer })
 end
 
-del_mappings_difficulty = function()
-    vim.keymap.del("n", "1")
-    vim.keymap.del("n", "2")
-    vim.keymap.del("n", "3")
-    vim.keymap.del("n", "4")
+del_mappings_difficulty = function(buffer)
+    vim.keymap.del("n", "1", { buffer = buffer })
+    vim.keymap.del("n", "2", { buffer = buffer })
+    vim.keymap.del("n", "3", { buffer = buffer })
+    vim.keymap.del("n", "4", { buffer = buffer })
 end
 
 return {
