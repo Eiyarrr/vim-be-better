40ccae095b71e8845ae819334311b0300510a728
Eiyarrr <eiyar@eiyar.com>
2026-07-23 01:56:47 -0700

    feat: made menu mapping control easier

--- lua/vim-be-better/menu.lua
+++ lua/vim-be-better/menu.lua
@@ -2,23 +2,13 @@ local engine = require("lua.vim-be-better.engine")
 local registry = require("lua.vim-be-better.modes.registry")
 local ui = require("lua.vim-be-better.ui")
 
-local function delMappings()
-    vim.keymap.del("n", "1")
-    vim.keymap.del("n", "2")
-end
+-- function predeclaration
+local set_mappings_mode
+local del_mappings_mode
+local set_mappings_difficulty
+local del_mappings_difficulty
 
-local function setMenuMappings()
-    vim.keymap.set("n", "1", function()
-        delMappings()
-        engine.state.mode = registry.hjkl
-    end)
-    vim.keymap.set("n", "2", function()
-        delMappings()
-        engine.state.mode = registry.hjkllegacy
-    end)
-end
-
-local function createMainMenu(buffer)
+local function create_main_menu(buffer)
     local menu = {
         "VimBeBetter",
         "",
@@ -34,6 +24,48 @@ local function createMainMenu(buffer)
     setMenuMappings()
 end
 
+set_mappings_mode = function()
+    vim.keymap.set("n", "1", function()
+        del_mappings_mode()
+        engine.state.mode = registry.hjkl
+    end)
+    vim.keymap.set("n", "2", function()
+        del_mappings_mode()
+        engine.state.mode = registry.hjkllegacy
+    end)
+end
+
+del_mappings_mode = function()
+    vim.keymap.del("n", "1")
+    vim.keymap.del("n", "2")
+end
+
+set_mappings_difficulty = function()
+    vim.keymap.set("n", "1", function()
+        del_mappings_difficulty()
+        engine.state.difficulty = "easy"
+    end)
+    vim.keymap.set("n", "2", function()
+        del_mappings_difficulty()
+        engine.state.difficulty = "medium"
+    end)
+    vim.keymap.set("n", "3", function()
+        del_mappings_difficulty()
+        engine.state.difficulty = "hard"
+    end)
+    vim.keymap.set("n", "4", function()
+        del_mappings_difficulty()
+        engine.state.difficulty = "challenge"
+    end)
+end
+
+del_mappings_difficulty = function()
+    vim.keymap.del("n", "1")
+    vim.keymap.del("n", "2")
+    vim.keymap.del("n", "3")
+    vim.keymap.del("n", "4")
+end
+
 return {
-    createMainMenu = createMainMenu,
+    create_main_menu = create_main_menu,
 }
